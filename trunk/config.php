fc67fd55b12a575487eccbd140a1126e93fb36d0
lokkju <lokkju@7d74bfde-2677-11df-9030-3bcc40d9c775>
2007-06-20 20:42:41 +0000

    Broke out all config items into config.php

---
+++ trunk/config.php
@@ -0,0 +1,17 @@
+<?php
+
+// Global configuration
+
+// Do we log errors?
+$cfg_log_errors = true;
+
+// your primary nameserver
+$cfg_primary_ns = "ns1.ifpdx.com";
+
+// Database connection information - we need to abstract out to a DB class for things other then mysql
+$cfg_db_name = "dns";
+$cfg_db_host = "localhost";
+$cfg_db_user = "dns";
+$cfg_db_pass = "password";
+
+?>
